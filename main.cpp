2103058cd9237acb7496a3506d800c66709a2c9b
saksham garg <59284238+sakshamceo@users.noreply.github.com>
2021-06-12 02:35:15 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Maximum and minimum of an array using minimum number of comparisons
 pair<long long, long long> getMinMax(long long a[], int n) {
     pair <long long , long long>res;
     int max=INT_MIN,min=INT_MAX;
