171702c39bbf0e7edc57c561b5f33650ffdda112
saksham garg <59284238+sakshamceo@users.noreply.github.com>
2021-06-11 21:02:59 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+pair<long long, long long> getMinMax(long long a[], int n) {
+    pair <long long , long long>res;
+    int max=INT_MIN,min=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>=max)
+        {
+            max=a[i];
+        }
+        if(a[i]<=min)
+        {
+            min=a[i];
+        }
+    }
+    res.first =min;
+    res.second=max;
+    return res;
